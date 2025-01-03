e0a36c15d0ff3b6e8cc56d8cfad465972c0a6152
Your Name <you@example.com>
2025-01-03 11:52:09 +0100

    manque quartiles pour descibe

--- describe.py
+++ describe.py
@@ -1,9 +1,82 @@
 import dstools as ds
+import pandas as pd
+import math
+
+def isNum(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+
+def columnParser(column: pd.Series):
+    try:
+        i = 0
+        for value in column:
+            if (isNum(value) == True):
+                if not pd.isna(value):
+                    i += 1
+            else:
+                return -1
+        return i
+    except Exception as e:
+        print(f"Error: {e}")
+
+def mean(column: pd.Series):
+    sum = 0
+    size = 0
+    for value in column:
+        if not pd.isna(value):
+            sum += value
+            size += 1
+    return sum / size
+
+def std(column: pd.Series, moyenne):
+    sum = 0
+    size = 0
+    for value in column:
+        if not pd.isna(value):
+            sum += (moyenne - value) ** 2
+            size += 1
+    return math.sqrt(sum / size)
+
+def min(column: pd.Series):
+    min = column[0]
+    for value in column:
+        if not pd.isna(value):
+            if value < min:
+                min = value
+    return min
+
+def max(column: pd.Series):
+    max = column[0]
+    for value in column:
+        if not pd.isna(value):
+            if value > max:
+                max = value
+    return max
 
 def main():
     try :
-        pass
-        
+        data = ds.load_csv('datasets/dataset_train.csv')
+        print(data.columns)
+        column = data['Transfiguration']
+
+        moy = mean(column)
+        count = columnParser(column)
+
+        print('count ', count)
+        print('mean ', moy)
+        print('std ', std(column, moy))
+        print('min', min(column))
+        print('max', max(column))
+
+        print('PANDAS')
+        print(column.describe())
+
+        # for column in data.columns:
+        #     print(column)
+        #     count = columnParser(data[column])
     except Exception as e:
         print(f"Error: {e}")
 
